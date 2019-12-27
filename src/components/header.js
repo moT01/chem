496bafa0dcc1db9e3b073b432b9d68dd2e7ddcc8
moT01 <tmondloch01@gmail.com>
2019-12-27 08:16:40 -0600

    feat: expand nav

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,15 @@ import { Link } from "gatsby"
 import './header.css'
 
 const Header = () => (
-	<div className='header'>
-		<Link className='header-logo' to='/'>HOME</Link>
+	<div className='header-wrap'>
+		<div className='header-logo'>LOGO</div>
+		<div className='header-links-wrap'>
+			<Link className='header-link' to='/'>about</Link>
+			<Link className='header-link' to='/'>research</Link>
+			<Link className='header-link' to='/'>publications</Link>
+			<Link className='header-link' to='/'>students</Link>
+			<Link className='header-link' to='/'>more</Link>
+		</div>
 	</div>
 );
 
